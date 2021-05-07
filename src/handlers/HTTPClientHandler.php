a0404f730b3f3d235eec0ef2dd9ea19e90a3d8f1
Nate Obray <nathanobray@gmail.com>
2021-05-07 23:39:30 +0000

    remove debug code

--- src/handlers/HTTPClientHandler.php
+++ src/handlers/HTTPClientHandler.php
@@ -30,24 +30,20 @@ class HTTPClientHandler implements \obray\interfaces\SocketClientHandlerInterfac
 
     public function onStart(\obray\SocketServer $connection): void
     {
-        print_r("Started client\n");
         $this->client = $connection;
     }
 
     public function onStartClient(\obray\SocketClient $connection): void
     {
-        print_r("Started client\n");
         $this->client = $connection;
     }
 
     public function onConnect(\obray\interfaces\SocketConnectionInterface $connection): void
     {
-        print_r("Connecting...");
     }
 
     public function onConnected(\obray\interfaces\SocketConnectionInterface $connection): void
     {
-        print_r("success\n");
         if(empty($this->headers)) $this->headers = new \obray\http\Headers([]);
         $this->headers->addHeader(new \obray\http\Header('Host', $this->host));
         $request = new \obray\http\Request($this->method, $this->uri, 'HTTP/1.1', $this->headers);
@@ -58,7 +54,7 @@ class HTTPClientHandler implements \obray\interfaces\SocketClientHandlerInterfac
 
     public function onConnectFailed(\obray\interfaces\SocketConnectionInterface $connection): void
     {
-        print_r("failed\n");
+        
     }
 
     public function onData(string $data, int $readLength, \obray\interfaces\SocketConnectionInterface $connection)
@@ -91,8 +87,6 @@ class HTTPClientHandler implements \obray\interfaces\SocketClientHandlerInterfac
                 // no content length found, check for chunked encodeing next
             }
             
-            print_r("empty line, end of headers\n");
-            exit();
         }
 
         // handle headers
@@ -110,23 +104,23 @@ class HTTPClientHandler implements \obray\interfaces\SocketClientHandlerInterfac
 
     public function onWriteFailed($data, \obray\interfaces\SocketConnectionInterface $connection): void
     {
-        print_r("Write to socket failed\n");
+        
     }
 
     public function onReadFailed(\obray\interfaces\SocketConnectionInterface $connection): void
     {
-        print_r("Read from socket failed\n");
+        
         exit();
     }
 
     public function onDisconnect(\obray\interfaces\SocketConnectionInterface $connection): void
     {
-        print_r("Disconnecting...");
+        
     }
 
     public function onDisconnected(\obray\interfaces\SocketConnectionInterface $connection): void
     {
-        print_r("Success\n");
+        
     }
 
     public function getHost(): string
